71a9427fdcc5a6ecaa36e13c2432dac033824236
romekkk29 <87077292+romekkk29@users.noreply.github.com>
2023-03-20 09:34:08 -0300

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -7,27 +7,7 @@ import os
 
   
 def ask_ai(promt,token):
-"""
-    os.environ["OPENAI_API_KEY"] = token
- # set maximum input size
-    max_input_size = 4096
-    # set number of output tokens
-    num_outputs = 300
-    # set maximum chunk overlap
-    max_chunk_overlap = 20
-    # set chunk size limit
-    chunk_size_limit = 600 
 
-    # define LLM
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003", max_tokens=num_outputs))
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
- 
-    documents = SimpleDirectoryReader('data').load_data()
-    
-    index = GPTSimpleVectorIndex(
-        documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper
-    )
-"""
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
     query='Responde en markdown. /markdown \n'+ promt
     response = index.query(query,response_mode="default",mode="embedding")
