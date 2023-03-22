138df071e4725e52b983eaeea063d3601a30b10c
romekkk29 <87077292+romekkk29@users.noreply.github.com>
2023-03-22 18:43:38 -0300

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -35,7 +35,7 @@ def construct_index(direct,token):
 def ask_ai(promt,token):
     os.environ["OPENAI_API_KEY"] = token
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
-    query='Eresun asistente virtual que responde usando el lenguaje markdown. \n'+ promt
+    query='Eres un asistente virtual que responde usando el lenguaje markdown. \n'+ promt
     response = index.query(query,response_mode="default",mode="embedding")
     return response.response
   
